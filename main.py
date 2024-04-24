f29bc30895bf1956f3e606abd03934d03579ac96
qriwty <arthur.social.rech@gmail.com>
2024-04-24 03:31:23 +0300

    Module update

--- main.py
+++ main.py
@@ -11,17 +11,13 @@ mavlink_connection = MAVLinkHandler("udp:0.0.0.0:14550")
 
 attitude_processor = AttitudeProcessor()
 gimbal_processor = GimbalProcessor()
-attitude_thread = DataAcquisitionThread(mavlink_connection, [attitude_processor, gimbal_processor])
-attitude_thread.start()
+
+acquisition_thread = DataAcquisitionThread(mavlink_connection, [attitude_processor, gimbal_processor])
+acquisition_thread.start()
 
 for _ in range(3):
     print(attitude_processor.queue.get_latest())
     print(gimbal_processor.queue.get_latest())
+
     time.sleep(1)
 
-
-# while True:
-#     message = mavlink_connection.receive_packet(MAVLinkDataType.GIMBAL.value)
-#
-#     gimbal = Gimbal.from_mavlink(message)
-#     print("GIMBAL", [math.degrees(rad) for rad in gimbal.quaternion.to_euler()])
